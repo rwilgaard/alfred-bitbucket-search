791419c6cafaf83d6fbfb592bf9061e7576fe1df
rwilgaard <rasmus@wilgaard.dk>
2023-02-12 21:54:00 +0100

    feat: add option to show repo commits

--- src/main.go
+++ src/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	aw "github.com/deanishe/awgo"
 	bb "github.com/rwilgaard/bitbucket-go-api"
+	"go.deanishe.net/fuzzy"
 )
 
 type workflowConfig struct {
@@ -18,23 +21,43 @@ type workflowConfig struct {
 }
 
 var (
-    wf          *aw.Workflow
-    authFlag    string
-    cacheFlag   bool
-    cacheName   = "repositories.json"
-    maxCacheAge = 1 * time.Hour
+    wf            *aw.Workflow
+    authFlag      string
+    cacheFlag     bool
+    commitFlag    bool
+    repoCacheName = "repositories.json"
+    maxCacheAge   = 1 * time.Hour
 )
 
 func init() {
-    wf = aw.New()
+    sopts := []fuzzy.Option{
+		fuzzy.AdjacencyBonus(10.0),
+		fuzzy.LeadingLetterPenalty(-0.1),
+		fuzzy.MaxLeadingLetterPenalty(-3.0),
+		fuzzy.UnmatchedLetterPenalty(-0.5),
+	}
+    wf = aw.New(
+        aw.SortOptions(sopts...),
+    )
     flag.StringVar(&authFlag, "auth", "", "authentication")
     flag.BoolVar(&cacheFlag, "cache", false, "cache repositories")
+    flag.BoolVar(&commitFlag, "commits", false, "show commits for repository")
+}
+
+func hasAll(input string, words []string) bool {
+    for _, w := range words {
+        if strings.Contains(input, w) {
+            continue
+        }
+        return false
+    }
+    return true
 }
 
 func getAllRepositories(api *bb.API) ([]*bb.RepositoryList, error) {
     query := bb.RepositoriesQuery{
         Limit: 9999,
-    } 
+    }
 
     repos, err := api.GetRepositories(query)
     if err != nil {
@@ -43,13 +66,13 @@ func getAllRepositories(api *bb.API) ([]*bb.RepositoryList, error) {
 
     var results []*bb.RepositoryList
     results = append(results, repos)
-    
+
     for !repos.IsLastPage {
         log.Println(repos.NextPageStart)
         query := bb.RepositoriesQuery{
             Limit: 9999,
             Start: int(repos.NextPageStart),
-        } 
+        }
         repos, err = api.GetRepositories(query)
         if err != nil {
             return nil, err
@@ -58,6 +81,31 @@ func getAllRepositories(api *bb.API) ([]*bb.RepositoryList, error) {
     }
 
     return results, nil
+}
+
+func getCommits(api *bb.API, projectKey string, repoSlug string) (*bb.CommitList, error) {
+    query := bb.CommitsQuery{
+        ProjectKey:     projectKey,
+        RepositorySlug: repoSlug,
+        
+    }
+
+    commits, err := api.GetCommits(query)
+    if err != nil {
+        return nil, err
+    }
+
+    return commits, nil
+}
+
+func addCredentialsPrompt(authType string) {
+    wf.NewItem("Credentials not configured...").
+        Subtitle("Press ⏎ to configure").
+        Icon(aw.IconInfo).
+        Arg("auth").
+        Var("auth_type", authType).
+        Valid(true)
+    wf.SendFeedback()
 }
 
 func run() {
@@ -75,37 +123,63 @@ func run() {
     authType := "API Token"
     token, err := wf.Keychain.Get(authType)
     if err != nil {
-        log.Println(err)
-        wf.NewItem("Credentials not configured...").
-            Subtitle("Press ⏎ to configure").
-            Icon(aw.IconInfo).
-            Arg("auth").
-            Var("auth_type", authType).
+        addCredentialsPrompt(authType)
+        return
+    }
+
+    cfg := &workflowConfig{
+        APIToken: token,
+    }
+
+    if err := wf.Config.To(cfg); err != nil {
+        wf.FatalError(err)
+    }
+
+    if cfg.Username == "" || cfg.URL == "" {
+        addCredentialsPrompt(authType)
+        return
+    }
+
+    api, err := bb.NewAPI(cfg.URL, cfg.Username, cfg.APIToken)
+    if err != nil {
+        wf.FatalError(err)
+    }
+
+    if commitFlag {
+        wf.Configure(aw.SuppressUIDs(true))
+        repoSlug := os.Getenv("repoSlug")
+        projectKey := os.Getenv("projectKey")
+        commits, err := getCommits(api, projectKey, repoSlug)
+        if err != nil {
+            wf.FatalError(err)
+        }
+
+        wf.NewItem("Go back").
+            Icon(aw.IconHome).
             Valid(true)
+
+        for _, c := range commits.Values {
+            t := time.UnixMilli(c.CommitterTimestamp).Format("02-01-2006 15:04")
+            wf.NewItem(c.Message).
+                Subtitle(fmt.Sprintf("%s  |  %s  |  %s", c.DisplayID, c.Committer.Name, t)).
+                Var("message", c.Message).
+                Valid(true)
+        }
+
         wf.SendFeedback()
         return
     }
 
-    cfg := workflowConfig{
-        APIToken: token,
-    }
-
-
     if cacheFlag {
         wf.Configure(aw.TextErrors(true))
         log.Printf("[cache] fetching repositories...")
-
-        api, err := bb.NewAPI(cfg.URL, cfg.Username, cfg.APIToken)
-        if err != nil {
-            wf.FatalError(err)
-        }
 
         repos, err := getAllRepositories(api)
         if err != nil {
             wf.FatalError(err)
         }
 
-        if err := wf.Cache.StoreJSON(cacheName, repos); err != nil {
+        if err := wf.Cache.StoreJSON(repoCacheName, repos); err != nil {
             wf.FatalError(err)
         }
 
@@ -114,13 +188,13 @@ func run() {
     }
 
     var repos []*bb.RepositoryList
-    if wf.Cache.Exists(cacheName) {
-        if err := wf.Cache.LoadJSON(cacheName, &repos); err != nil {
+    if wf.Cache.Exists(repoCacheName) {
+        if err := wf.Cache.LoadJSON(repoCacheName, &repos); err != nil {
             wf.FatalError(err)
         }
     }
 
-    if wf.Cache.Expired(cacheName, maxCacheAge) {
+    if wf.Cache.Expired(repoCacheName, maxCacheAge) {
         wf.Rerun(0.3)
         if !wf.IsRunning("cache") {
             cmd := exec.Command(os.Args[0], "-cache")
@@ -141,18 +215,33 @@ func run() {
 
     for _, list := range repos {
         for _, repo := range list.Values {
-            wf.NewItem(repo.Name).
-                Subtitle(repo.Project.Name)
+            // e := fmt.Sprintf("%s %s %s %s", repo.Name, repo.Slug, repo.Project.Name, repo.Project.Key)
+            // if !hasAll(strings.ToLower(e), strings.Split(strings.ToLower(query), " ")) {
+            //     continue
+            // }
+            it := wf.NewItem(repo.Name).
+                Subtitle(fmt.Sprintf("%s", repo.Project.Name)).
+                Match(fmt.Sprintf("%s %s %s %s", repo.Name, repo.Slug, repo.Project.Name, repo.Project.Key)).
+                Var("projectKey", repo.Project.Key).
+                Var("repoSlug", repo.Slug).
+                Var("link", repo.Links["self"][0].Href).
+                Var("query", query).
+                Valid(true)
+
+            it.NewModifier(aw.ModOpt).
+                Subtitle("Show commits").
+                Arg("commits").
+                Valid(true)
         }
     }
 
     wf.Filter(query)
 
-	if wf.IsEmpty() {
+    if wf.IsEmpty() {
         wf.NewItem("No results found...").
             Subtitle("Try a different query?").
             Icon(aw.IconInfo)
-	}
+    }
     wf.SendFeedback()
 }
 
